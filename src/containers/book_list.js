0ebf70e266ce1d795e019565165ee7f36b45f647
Jesse <jessehwang86@yahoo.com>
2017-10-24 12:57:52 -0400

    consumed book detail action to reducer

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -7,10 +7,8 @@ class BookList extends Component {
 	renderList() {
 		return this.props.books.map((book) => {
 			return (
-				<li 
-					key={ book.title } 
-					onClick={() => this.props.selectBook(book)}
-					className="list-group-item">{book.title}
+				<li key={ book.title } onClick={ () => this.props.selectBook(book) } className='list-group-item'>
+					{book.title}
 				</li>
 			)
 		})
